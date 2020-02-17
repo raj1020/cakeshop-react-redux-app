2552728c5ca5eb0258d58448b789c10b54b47eae
raj1020 <raj.1020@hotmail.com>
2020-02-17 23:33:08 +1100

    added reducer for iceCream sales. combined two reducers as 'rootReducer'

--- src/redux/iceCream/iceCreamReducer.js
+++ src/redux/iceCream/iceCreamReducer.js
@@ -5,5 +5,14 @@ const initialState = {
     numOfIceCreams: 20
 }
 const iceCreamReducers = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
+        }
 
+        default: return state;
+    }
 };
+
+export default iceCreamReducer;
